ea8bdc1466512523fe5f1dfdec6de0bea575af37
neilkuan <guan840912@gmail.com>
2022-09-04 03:48:24 +0000

    chore(deps): upgrade dependencies

--- .projenrc.js
+++ .projenrc.js
@@ -8,17 +8,17 @@ const project = new awscdk.AwsCdkConstructLibrary({
   author: 'Neil Kuan',
   // define install helm step in build and release workflows.
   release: false,
-  workflowBootstrapSteps: [
-    {
-      name: 'Install Helm',
-      id: 'install_helm',
-      run: `curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3
-chmod 700 get_helm.sh
-./get_helm.sh
-helm repo add eks https://aws.github.io/eks-charts
-helm repo update`,
-    },
-  ],
+  //   workflowBootstrapSteps: [
+  //     {
+  //       name: 'Install Helm',
+  //       id: 'install_helm',
+  //       run: `curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3
+  // chmod 700 get_helm.sh
+  // ./get_helm.sh
+  // helm repo add eks https://aws.github.io/eks-charts
+  // helm repo update`,
+  //     },
+  //   ],
   //   releaseWorkflowSetupSteps: [{
   //     name: 'Install Helm',
   //     id: 'install_helm',
